e99f54d33669e94bfc26b20f2d10b4ac2b63d02b
xuchengvcc <xucheng66666@gmail.com>
2024-09-27 10:17:15 +0000

    readme

--- README.md
+++ README.md
@@ -5,16 +5,50 @@ to generate `name_grpc.pb.go` and `name.pb.go`
 ```
 protoc --go_out=. --go-grpc_out=. name.proto
 ```
+# configuration
 
-# test single client
+Please first go to the `./conf` folder to set the address information: `server.json` and `proxy.json`
 
+cluster info:
+```
+# sever.json
+
+{
+    "nodeNum":5,
+    "SyncAddr":"127.0.0.1:9999", // cluster manager moniter port
+    "ServAddr":"127.0.0.1:9998", // cluster manager serve port
+    "servers":[
+        {
+            // node info
+            "Name":"node1",
+            "SerAddr":"127.0.0.1:50000",
+            "NodeAddr":"127.0.0.1:50030",
+            "SyncAddr":"127.0.0.1:50050",
+        },
+        ......
+    ]
+}
+```
+
+proxy info:
 
 ```
-cd server
-./run_tests.sh -cp CPU.out -f TestBasic
+// proxy.json
+{
+    "GetAddr":"127.0.0.1:9000", // read request port
+    "PutAddr":"127.0.0.1:9001", // write request port
+}
 ```
 
-# CPU Utilization
+
+# start a server cluster
+
 ```
-go tool pprof CPU.out
+cd ./main
+go run main.go -s server
+```
+
+# start a client
+```
+go run main.go -s client
 ```
--- main/main.go
+++ main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"raft-kv-service/conf"
@@ -10,9 +11,69 @@ import (
 	"raft-kv-service/server"
 	"strconv"
 	"strings"
+	"time"
 )
 
-func start() {
+func main() {
+	var server_or_client string
+
+	flag.StringVar(&server_or_client, "s", "server", "启动 service: server, 启动 client: client")
+	flag.Parse()
+	switch server_or_client {
+	case "server":
+		StartServer()
+	case "client":
+		StartClient()
+	default:
+		mylog.DPrintln("Not Support ", server_or_client, ", only support [server or client]")
+	}
+}
+
+func StartClient() {
+	globalObj := conf.GetGlobalObj()
+
+	client := server.MakeClerk(globalObj.ProxyAddrs)
+	defer client.Stop()
+
+	timeout := 3 * time.Second
+	var Op int32 = 0
+	var Key string
+	var Value string
+	var Got string
+	var err error
+	for Op != -1 {
+		fmt.Println("please enter Op: [1: GET, 2: PUT, 3: APPEND, -1: Quit]")
+		fmt.Scan(&Op)
+		if Op == -1 {
+			break
+		}
+		fmt.Println("please enter Key: [any string]")
+		fmt.Scan(&Key)
+		switch Op {
+		case 1:
+			Got, err = client.Get(Key, timeout)
+		case 2:
+			fmt.Println("please enter Value: [any string]")
+			fmt.Scan(&Value)
+			err = client.Put(Key, Value, timeout)
+		case 3:
+			fmt.Println("please enter Value: [any string]")
+			fmt.Scan(&Value)
+			err = client.Append(Key, Value, timeout)
+		}
+		if err != nil {
+			fmt.Println("Process error: ", err)
+			continue
+		}
+		if Op == 1 {
+			fmt.Println("Process OK, you get: ", Got)
+		} else {
+			fmt.Println("Process OK")
+		}
+	}
+}
+
+func StartServer() {
 
 	globalObj := conf.GetGlobalObj()
 
